73621c08cfd0ca8fa41c5fa69377e311350f1e62
YNTKTS-3 <112832863+YNTKTS-3@users.noreply.github.com>
2023-05-21 17:18:34 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -21,15 +21,43 @@ global.APIKeys = {
    'https://api.zeeoneofc.my.id': 'V12', // 👉 login https://api.zeeoneofc.my.id to get apikey
 }
 global.sewabotnya = `
-edit sewabot di file settings.js line 24
+*SEWA BOT*
+-> *Rp6.500/Bulan*
+-> *Rp11.500/3 Bulan*
+-> *(+Rp 1.500 = biaya admin)
+-> *Req berapa Bulan? Silahkan*
+-> *Rp101.500 (boleh cicil, 0% bunga) = selamanya + klo bot ganti nomor ga usah bayar lagi. Kalo owner lupa, kalian bisa chatting ke owner*
+
+*Keuntungan Sewabot*
+-> *Fitur Grup Banyak*
+-> *Fitur Lumayan Banyak*,
+-> *Bot Fast Respon (ga kayak respon dia ke aku)*
+-> *On 24 Jam Menjaga Grup tetap aman*
+
+*BUY PREMIUM*
+-> *Rp7.000/bulan*
+-> *Rp10.000/1,5 bulan (45 hari)*
+-> *Request harga? Silahkan*
+
+*Keuntungan Premium*
+-> *Akses All Fitur*
+-> *Limit Unlimited (sudah termasuk limit games)*
+
+*BUY OTP APK WA DLL*
+-> *Rp 10.000/1 nomer
+-> *Rp 15.000/2 nomer
+Minat? Silahkan chat owner ku: WA.me/6285693460387?
+
+Note: Jika .menu error, silahkan ketik .allmenu
+Jangan lupa join grup bot: https://bit.ly/MAJAPAHIT_EMPIRE
 `
 // setting 
 global.firtsChat = true //send firts message to new user
 global.autoread = true // auto read message
 global.antidelete = false // anti delete
 global.antiviewonce = true // anti view once
-global.autobio = false //auto ganti bio
-global.anticall = true //anti call, jika true maka org yg nelpon bot auto di blokir
+global.autobio = true //auto ganti bio
+global.anticall = false //anti call, jika true maka org yg nelpon bot auto di blokir
 global.userRegister = false
 global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
 global.autolevelup = true
@@ -38,7 +66,7 @@ global.auto_antilink2 = true  //jika true, bot (admin) otomatis menghapus pesan 
 global.auto_badword = false //jika true, bot (admin) otomatis menghapus pesan & mengeluarkan member yg mengirim kata kasar
 global.auto_badword2 = true  //jika true, bot (admin) otomatis menghapus pesan member yang mengirim kata kasar
 global.auto_antiwame = false  //jika true, bot (admin) otomatis menghapus pesan & mengeluarkan member yang mengirim link grup
-global.auto_antiwame2 = true  //jika true, bot (admin) otomatis menghapus pesan member yang mengirim link grup
+global.auto_antiwame2 = false  //jika true, bot (admin) otomatis menghapus pesan member yang mengirim link grup
 global.autowelcome = true //auto welcome msg all group
 global.autodetectpromote = true //auto detect admin all group
 global.autodetectdemote = true //auto detect un admin all group
@@ -54,27 +82,27 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Terizlaོ" //namabot kalian
+global.ownername = "ᴹᴿ᭄Lemon Teaོ ×፝֟͜×" //nama kalian
+global.myweb = "https://bit.ly/MAJAPAHIT_EMPIRE" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/@gataukoktanyagw549" //bebas asal jan hapus
+global.myweb2 = "https://saweria.co/AIDIT" //bebas
+global.email = "masasih@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Makassar' //  timezone wib
+global.timezone = 'Asia/Jakarta' //  timezone wib
 global.premium_fitur = ["ai", "openai", "chatgpt"] //tambahin aja sendiri, soalnya gk semua org mau fiturnya premium jadi gw buat kyk gini aja, biar bisa custom fitur mana aja yang bakal di jadiin premium
-global.badword_list = ["asu", "anjir"] //tambahin sendiri kata² nya, gunakan huruf kecil
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62887435047326"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = owner[0] + "@s.whatsapp.net"
-global.ownernomerr = "+"+ownernomer //tidak perlu di edit
+global.badword_list = ["asu","anjir","anj","anjay","anjrot","konto","kontol","bangsat","bangsad","bgst","bgsd"] //tambahin sendiri kata² nya, gunakan huruf kecil
+global.premium = ['6285693460387','62895709850707'] //premium user
+global.owner = ["6285693460387","62881027432241","62895709850707"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285693460387" // nomor wa kalian
+global.ownernomerr = "+"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.tag_owner = "./tag_owner.mp3" // audio ini akan di kirim apabila ada member yang menandai (tag) owner bot 
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© ' + botname //sticker wm ubah
-global.author = 'Created By ' + ownername //sticker wm ganti nama kalian
+global.packname = 'Made By +62 881-5458-143 ' + botname //sticker wm ubah
+global.author = 'Creator bot WA: +62 856-9346-0387 ' + ownername //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.push_kontak_delay = 5000 //5 detik, 1000 = 1 detik
 //type menu for menu
